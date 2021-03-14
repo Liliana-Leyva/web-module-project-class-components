9dd7bc3432b93289c2017798f7d7f146c9372303
Liliana Leyva <ingeleyva@yahoo.com>
2021-03-14 18:02:04 -0500

    input and add

--- src/components/TodoForm.js
+++ src/components/TodoForm.js
@@ -1,1 +1,29 @@
-//
+//Todo Form
+import React from 'react'
+
+class TodoList extends React.Component{
+ constructor(){
+    super();
+    this.state={
+        todoName: ''
+    }
+ }
+ 
+ handleChanges= e =>{
+    this.setState({todoName: e.target.value})
+ }
+ handleSubmit = e => {
+    e.preventDefault();
+    this.props.addItem(this.state.todoName);
+ }
+ render(){
+    return(
+        <form onSubmit={this.handleSubmit}>
+            <input onChange={this.handleChanges} value ={this.state.todoName} type="text" name="item"/>
+            <button>Add</button>
+        </form>
+    )
+ }
+}
+
+export default TodoList
--- src/components/TodoList.js
+++ src/components/TodoList.js
@@ -1,2 +1,30 @@
 // your components will all go in this `component` directory.
 // feel free to change this component.js into TodoList.js
+import React from 'react'
+
+class TodoList extends React.Component{
+ constructor(){
+    super();
+    this.state={
+        todoName: ''
+    }
+ }
+ 
+ handleChanges= e =>{
+    this.setState({todoName: e.target.value})
+ }
+ handleSubmit = e => {
+    e.preventDefault();
+    this.props.addItem(this.state.todoName);
+ }
+ render(){
+    return(
+        <form onSubmit={this.handleSubmit}>
+            <input onChange={this.handleChanges} value ={this.state.todoName} type="text" name="item"/>
+            <button>Add</button>
+        </form>
+    )
+ }
+}
+
+export default TodoList
